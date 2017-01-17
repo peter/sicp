0f62b0fe6c7601f2cb5e8d2f2efeba0551953e7e
Peter Marklund <peter@marklunds.com>
2017-01-17 11:40:00 +0100

    Solutions until 1.7

--- README.md
+++ README.md
@@ -7,19 +7,26 @@ Solutions to exercises and notes accompanying the [SICP course](https://mitpress
 On a mac you can install Scheme with:
 
 ```
+brew install rlwrap
 brew install mit-schema
 ```
 
 To start the REPL:
 
 ```
-mit-scheme
+rlwrap mit-scheme
 ```
 
 To load a file:
 
 ```
-mit-scheme --load solutions/ch1.scm
+rlwrap mit-scheme --load solutions/ch1.scm
+```
+
+Hello World:
+
+```scheme
+(write "Hello world")
 ```
 
 ## 1.1 The Elements of Programming
@@ -31,6 +38,26 @@ Every powerful programming language has three fundamental mechanisms:
 * Abstractions
 
 In programming we deal with procedures and data.
+
+### 1.1.7  Example: Square Roots by Newton's Method
+
+```scheme
+(define (square-root x)
+  (define (good-enough? guess x)
+    (< (abs (- (square guess) x))
+       0.01))
+  (define (average a b)
+    (/ (+ a b) 2))
+  (define (improve guess x)
+    (average guess (/ x guess)))
+  (define (try guess x)
+    (if (good-enough? guess x)
+      guess
+      (try (improve guess x) x)))
+  (try 1 x))
+
+(square-root 9) ; => 3.00009155413138
+```
 
 ## Resources
 
--- solutions/ch1.scm
+++ solutions/ch1.scm
@@ -1,5 +1,3 @@
-; (write "Hello world")
-
 ; 1.1
 
 10
@@ -31,6 +29,7 @@
 ; Define a procedure that takes three numbers as arguments
 ; and returns the sum of the squares of the two larger numbers.
 ; 1.3
+
 (define (sum-square-two a b c)
   (cond ((<= a b c) (+ (square b) (square c)))
         ((<= b a c) (+ (square a) (square c)))
@@ -54,9 +53,55 @@
 ; evaluation model on the other hand does not have this problem as the evaluation
 ; of p is postponed and ends up not being needed and thus never happens in this case.
 
-(define (p) (p))
-(define (test x y)
-  (if (= x 0)
-    0
-    y))
-(test 0 (p))
+;(define (p) (p))
+;(define (test x y)
+;  (if (= x 0)
+;    0
+;    y))
+;(test 0 (p))
+
+; 1.6
+
+; The new-if function causes an eternal loop as the else clause will always evalute
+
+; 1.7
+
+; For very large numbers the floating point precision capacity of the language
+; will not be enough and for small numbers the precision of the result will not be enough
+; "Stop when the change is a very small fraction of the guess"
+
+(define (square-root-precise x)
+  (define (good-enough? prev-guess guess x)
+    (< (/ (abs (- guess prev-guess)) guess)
+       0.001))
+  (define (average a b)
+    (/ (+ a b) 2.0))
+  (define (improve guess x)
+    (average guess (/ x guess)))
+  (define (try prev-guess guess x)
+    (if (good-enough? prev-guess guess x)
+      guess
+      (try guess (improve guess x) x)))
+  (try 1 (improve 1 x) x))
+
+(square-root-precise 9) ; => 3.000000001396984
+(square-root-precise 1000000) ; => 1000.0001533016629
+(square-root-precise 0.001) ; => 0.03162278245070105
+
+(define (square-root x)
+  (define (good-enough? guess x)
+    (< (abs (- (square guess) x))
+       0.01))
+  (define (average a b)
+    (/ (+ a b) 2.0))
+  (define (improve guess x)
+    (average guess (/ x guess)))
+  (define (try guess x)
+    (if (good-enough? guess x)
+      guess
+      (try (improve guess x) x)))
+  (try 1 x))
+
+(square-root 9) ; => 3.00009155413138
+(square-root 1000000) ; => 1000.0000000000118
+(square-root 0.001) ; => 0.06772532736082602
